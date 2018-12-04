a021aff51c8efcbfe0ecfc77abe19668356a84ba
jtmar28 <justinmartinez0123@gmail.com>
2018-12-03 23:58:18 -0500

    Added function to see repository, clear profile and show alert

--- ui.js
+++ ui.js
@@ -30,4 +30,57 @@ class UI{
             </div>
         `;
     }
+    //Show user repos
+    showRepos(repos){
+        let output = '';
+        repos.forEach(function(repo){
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-secondary">Public Repos: ${repo.watchers_count}</span>
+                            <span class="badge badge-success">Followers: ${repo.forks_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        document.getElementById('repos').innerHTML = output;
+    }
+    //Clear input
+    clearProfile(){
+        this.profile.innerHTML = '';
+    }
+    //Show Alert Message
+    showAlert(message,className){
+        //Clear any remaining alerts
+        this.clearAlert();
+        //Create div
+        const div = document.createElement('div');
+        //Add Class name
+        div.className = className;
+        //add Text
+        div.appendChild(document.createTextNode(message));
+        //Get Parent
+        const container = document.querySelector('.searchContainer');
+        //Get search box
+        const search = document.querySelector('.search');
+        //Insert Alert 
+        container.insertBefore(div,search);
+        //Timeout after 3 seconds
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+    //Clear Alery message
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert');
+        if(currentAlert){
+            currentAlert.remove();
+        }
+    }
 }
